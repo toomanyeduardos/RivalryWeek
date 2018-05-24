92b66d6aff180376b91eb66ebbf13f73e9a9865f
Eduardo Flores <eflores@ancestry.com>
2018-05-24 16:21:35 -0600

    broken checkbox

--- lib/teamSelection.dart
+++ lib/teamSelection.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:rivalry_week/rivalryAppWrapper.dart';
 
-class TeamSelection extends StatelessWidget {
+class TeamSelection extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return new TeamSelectionState();
+  }
+}
+
+class TeamSelectionState extends State<TeamSelection> {
+  bool checkboxValue = false;
 
   @override
   Widget build(BuildContext context) {
     return new RivalryAppWrapper(
-      body: new Text("some text"),
       appTitle: "why??",
+      body: new ListView(
+        children: <Widget>[
+          getRow("University of Utah"),
+        ],
+      ),
     );
   }
 
+  Row getRow(String label) {
+    return new Row(children: <Widget>[
+      new Checkbox(
+          value: checkboxValue,
+          onChanged: (bool value) {
+            setState(() {
+              checkboxValue = value;
+            });
+          }),
+      new Text(label)
+    ]
+    );
+  }
 }
