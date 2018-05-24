b30a79c7780fcb7d19d5ba1e6f2452f8595dc10f
Eduardo Flores <eflores@ancestry.com>
2018-05-24 13:42:28 -0600

    barebones basic sign in screen - doesn't do anything

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Rivalry Week'),
     );
   }
 }
@@ -89,21 +89,27 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            new TextFormField(
+              decoration: new InputDecoration(
+                labelText: "Enter your username"
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new TextFormField(
+              decoration: new InputDecoration(
+                  labelText: "Enter your password"
+              ),
+              obscureText: true
+            ),
+            new RaisedButton(
+                onPressed: () {
+                  print("Sign in button pressed!");
+                },
+                child: const Text("Sign in!"),
+                color: Colors.blueAccent,
             ),
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
