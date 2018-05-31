03475b780d1a84c10303b5a78c1f0d98024b7425
Eduardo Flores <eflores@ancestry.com>
2018-05-31 14:13:45 -0600

    got checkboxlisttile to work

--- lib/teamSelection.dart
+++ lib/teamSelection.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:rivalry_week/rivalryAppWrapper.dart';
 
 class TeamSelection extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return new TeamSelectionState();
@@ -10,10 +8,15 @@ class TeamSelection extends StatefulWidget {
 }
 
 class TeamSelectionState extends State<TeamSelection> {
-  bool checkboxValue = false;
+
+  Map<String, bool> _checkBoxValues = {
+    'University of Utah': false,
+    'BYU': false
+  };
 
   @override
   Widget build(BuildContext context) {
+    /* CHECKBOX NOT WORKING WHEN CALLED FROM THE WRAPPER - FIND OUT WHY?
     return new RivalryAppWrapper(
       appTitle: "why??",
       body: new ListView(
@@ -22,19 +25,25 @@ class TeamSelectionState extends State<TeamSelection> {
         ],
       ),
     );
-  }
+  */
 
-  Row getRow(String label) {
-    return new Row(children: <Widget>[
-      new Checkbox(
-          value: checkboxValue,
-          onChanged: (bool value) {
-            setState(() {
-              checkboxValue = value;
-            });
-          }),
-      new Text(label)
-    ]
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Some new title"),
+      ),
+      body: new ListView(
+        children: _checkBoxValues.keys.map((String key) {
+          return new CheckboxListTile(
+            title: new Text(key),
+            value: _checkBoxValues[key],
+            onChanged: (bool value) {
+              setState(() {
+                _checkBoxValues[key] = value;
+              });
+            },
+          );
+        }).toList(),
+      ),
     );
   }
 }
